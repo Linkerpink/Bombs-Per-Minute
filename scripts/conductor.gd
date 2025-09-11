fbd0a6ca426d5cbe38bd9240feb50fe5c9f6fb8a
Linkerpink <noahvanuunen2007@gmail.com>
2025-09-11 23:14:32 +0200

    time signatures and beat calculation

---
+++ scripts/conductor.gd
@@ -0,0 +1,86 @@
+extends AudioStreamPlayer
+
+#region Variables
+@export var song_bpm : int = 120 # The beats per minute in the song
+@export var song_measures : int = 4 # The amount of measures in a beat. For a 4/4 time signature, this would be 4. You can also make this 8 for more precice mapping capabilities
+@export var song_audio : AudioStream
+
+var song_position = 0.0
+var song_position_in_beats = 1
+var seconds_per_beat
+var last_reported_beat = 0
+var beats_before_start = 0
+var current_measure = 0
+
+var closest = 0
+var time_off_beat = 0.0
+
+@onready var start_timer : Timer = $"Start Timer"
+
+signal beat(position)
+signal measure(position)
+
+
+@onready var song_position_in_beats_text : RichTextLabel = $"../Song UI/MarginContainer/Debug UI/Song Position In Beats Text"
+@onready var current_measure_text : RichTextLabel = $"../Song UI/MarginContainer/Debug UI/Current Measure Text"
+#endregion
+
+func _ready() -> void:
+	seconds_per_beat = 60.0 / song_bpm
+	if song_audio != null:
+		stream = song_audio
+	else:
+		push_error("No song audio selected for this song!")
+	
+func _physics_process(delta: float) -> void:
+	if playing:
+		song_position = get_playback_position() + AudioServer.get_time_since_last_mix()
+		song_position -= AudioServer.get_output_latency()
+		song_position_in_beats = int(floor(song_position / seconds_per_beat)) + beats_before_start
+		_report_beat()
+		_update_ui()
+
+func _report_beat():
+	if last_reported_beat < song_position_in_beats:
+		if current_measure > song_measures - 1:
+			current_measure = 0
+		emit_signal("beat", song_position_in_beats)
+		emit_signal("measure", current_measure)
+		last_reported_beat = song_position_in_beats
+		current_measure += 1
+
+func play_with_beat_offset(_num):
+	beats_before_start = _num
+	start_timer.wait_time = seconds_per_beat
+	start_timer.start()
+
+func play_from_beat(_beat, _offset):
+	play()
+	seek(_beat * seconds_per_beat)
+	beats_before_start = _offset
+	current_measure = _beat % song_measures
+
+func _on_beat(position: Variant) -> void:
+	#print("beat")
+	pass
+
+func _on_measure(position: Variant) -> void:
+	#print(current_measure)
+	pass
+
+func _on_start_timer_timeout() -> void:
+	song_position_in_beats += 1
+	if song_position_in_beats < beats_before_start - 1:
+		start_timer.start()
+	elif song_position_in_beats == beats_before_start -1:
+		start_timer.wait_time = start_timer.wait_time - (AudioServer.get_time_to_next_mix() + AudioServer.get_output_latency())
+		start_timer.start()
+	else:
+		play()
+		start_timer.stop()
+	_report_beat()
+	
+func _update_ui():
+	song_position_in_beats_text.text = "Song position in beats: " + str(song_position_in_beats)
+	current_measure_text.text = "Current measure: " + str(current_measure)
+	pass
--- scripts/resource scripts/map.gd
+++ scripts/resource scripts/map.gd
@@ -4,6 +4,6 @@ class_name Map
 @export var name : String = "Map Name"
 @export var audio : AudioStream
 @export var bpm : float = 120
-@export var time_signature : Dictionary # For example 4/4 would be: Key = 4, Value = 4. Both are ints
+@export var measures : int
 @export var scroll_speed : float = 10
 @export var notes : Dictionary # Key (int) = lane, Value(float) = time Notes defined as (measure, beat, subdivision)
